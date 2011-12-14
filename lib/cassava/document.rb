5df240db6936a568d38b0849897737dd321a00af
Kurt Stephens <ks.github@kurtstephens.com>
2011-12-14 12:30:54 -0600

    Initial.

---
+++ lib/cassava/document.rb
@@ -0,0 +1,126 @@
+require 'csv'
+
+module Cassava
+  class Document
+    attr_accessor :name, :columns, :column_offset, :offset_column
+    attr_accessor :rows
+
+    def initialize opts = nil
+      @opts = opts
+      @name = opts[:name] or raise ArgumentError, "name not specified"
+      @columns = opts[:columns]
+      @column_offset = { }
+      @offset_column = [ ]
+      @ncols = 0
+      @rows = [ ]
+      @index = [ ]
+    end
+
+    def nrows
+      @rows.size
+    end
+
+    def empty_rows!
+      @rows = [ ]
+      @index.clear
+      self
+    end
+
+    def append_rows! rows
+      rows = rows.rows if Document === rows
+      @rows.concat(rows)
+      rows.each do | r |
+        @ncols = r.size if @ncols < r.size
+      end
+      @index.clear
+      self
+    end
+
+    def parse!
+      # debugger if $DEBUG
+      csv = nil
+      if RUBY_VERSION =~ /^1\.8/
+        @rows = [ ]
+        csv = CSV.open(name, "rb", @opts[:col_sep]) do | r |
+          @rows << r
+        end
+      else
+        csv_opts = { }
+        csv_opts[:col_sep] = @opts[:col_sep] if @opts[:col_sep]
+        csv = CSV.open(name, "rb", csv_opts)
+        @rows = csv.read
+      end
+
+      @columns = @rows.shift unless @columns
+      @ncols = @columns.size
+      i = -1
+      @columns.map! do | c |
+        i += 1
+        c = c.to_s
+        next if c.empty?
+        c = c.to_sym
+        @column_offset[c] = i
+        @offset_column[i] = c
+        c
+      end
+      row_i = 0
+      @rows.map! do | r |
+        @ncols = r.size if @ncols < r.size
+        h = { :_row_i => (row_i += 1) }
+        @column_offset.each do | c, i |
+          h[c] = r[i] if c && i
+        end
+        h
+      end
+      # debugger
+      self
+    ensure
+      csv.close if csv
+    end
+
+    def add_column! c
+      c = c.to_sym
+      unless i = @offset_column[c]
+        i = @ncols
+        @ncols += 1
+        @columns << c
+        @offset_column[c] = i
+        @column_offset[i] = c
+      end
+      i
+    end
+
+    def index! c
+      c = @column_offset[c] if Symbol === c
+      unless ind = @index[c] ||= { }
+        ind = @index[c] = { }
+        @rows.each do | r |
+          (ind[r[c]] ||= [ ]) << r
+        end
+      end
+      ind
+    end
+
+    def get c, v
+      index!(c)[v]
+    end
+
+    def emit! document
+      CSV.open(document, "wb") do | out |
+        a = @offset_column.map do | c |
+          c && c.to_s
+        end
+        out << a
+        @rows.each do | r |
+          a = @offset_column.map do | i |
+            i && r[i]
+          end
+          out << a
+        end
+      end
+      self
+    end
+  end
+end
+
+
---
+++ lib/cassava/main.rb
@@ -0,0 +1,101 @@
+require 'cassava/document'
+
+module Cassava
+  class Main
+    attr_accessor :progname
+    attr_accessor :args, :cmd, :opts, :exit_code
+    attr_accessor :result
+
+    def initialize args = nil
+      @progname = File.basename($0)
+      @args = (args || ARGV).dup
+      @opts = { }
+      @exit_code = 0
+    end
+
+    def run!
+      @cmd = @args.shift
+      sel = :"_#{cmd}!"
+    raise ArgumentError, "Invalid command: #{cmd.inspect}" unless respond_to?(sel)
+      send(sel)
+      result.emit!("/dev/stdout") if result
+      self
+    rescue ::Exception => exc
+      $stderr.puts "#{progname}: ERROR: #{exc.inspect}"
+      $stderr.puts "  #{exc.backtrace * "\n  "}" if $DEBUG
+      @exit_code = 1
+      self
+    end
+
+    def next_document!
+      doc = nil
+      opts = { }
+      until args.empty?
+        case arg = args.shift
+        when '-FS'
+          opts[:col_sep] = args.shift
+        when '-IGNORE'
+          opts[:ignore] = args.shift
+        when '-C'
+          opts[:columns] = args.shift.split(/\s*,\s*|\s+/)
+        else
+          opts[:name] = arg
+          doc = Document.new(opts)
+          doc.parse!
+          breakg
+        end
+      end
+      doc
+    end
+
+    def _cat!
+      @result = next_document!
+      until args.empty?
+        other = next_document!
+        other.columns.each { | c | result.add_column!(c) }
+        result.rows.append(other.rows)
+      end
+    end
+
+    def _select!
+      @result = next_document!
+      until args.empty?
+        new_result = result.dup
+        new_result.empty_rows!
+        key = args.shift.to_sym
+        val = args.shift
+        result.rows.each do | r |
+          new_result.append_rows!(left.get(key, val))
+        end
+        @result = new_result
+      end
+    end
+
+    def _join!
+      @result = next_document!
+      until args.empty?
+        left_key = args.shift.to_sym
+        right_key = args.shift.to_sym
+        right = next_document!
+        new_result = Document.new
+        result.columns.each do | c |
+          new_result.add_column!(c)
+        end
+        right.columns.each do | c |
+          new_result.add_columns!(c)
+        end
+        result.rows.each do | lr |
+          rrows = right.get(right_key, lr[left_key])
+          unless rrows.empty?
+            r = lr.dup
+            rrows.each do | rr |
+              r.merge(rr)
+            end
+            new_result.rows << r
+          end
+        end
+        @result = new_result
+      end
+    end
+  end
+end
